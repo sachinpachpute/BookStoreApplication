3664bb02d3b0450dc40ceb6c1f60b2019e7ef4cb
Sachin Pachpute <sachin.pachpute@gmail.com>
2023-01-25 04:55:38 +0000

    Eureka Discovery and API Gateway working fine

--- catalog-service/src/main/java/com/sp/spring/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sp/spring/catalog/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.sp.spring.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.sp.spring")
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
